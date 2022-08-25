0190df224d4e3f3f95c65b8494c2823864018eb3
dhanushk-dhanush <dhanushkumarkum@maveric-systems.com>
2022-08-25 15:30:50 +0530

    Transaction happy path commit

---
+++ src/main/java/com/maveric/transactionservice/controller/TransactionController.java
@@ -0,0 +1,17 @@
+package com.maveric.transactionservice.controller;
+
+import com.maveric.transactionservice.dto.TransactionDto;
+import com.maveric.transactionservice.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TransactionController {
+
+
+}
